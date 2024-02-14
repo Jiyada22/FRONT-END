3fb1fefb0630e848ffaf70d0002645b6e1003fee
Jiyada22 <jiyadakreenawong@gmail.com>
2024-02-14 22:27:51 +0700

    s

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 var bodyParser = require("body-parser");
 
-const base_url = "http://localhost:3000";
+const base_url = "http://10.104.14.241";
 //"body-parser": "^1.20.2",
 
 app.set("views", path.join(__dirname,"/public/views"));
